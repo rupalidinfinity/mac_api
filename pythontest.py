051a10cf3037572da719337803f29c9f7cd9cb96
rupalidinfinity <42231075+rupalidinfinity@users.noreply.github.com>
2019-06-27 12:50:52 +0530

    Update pythontest.py

--- pythontest.py
+++ pythontest.py
@@ -1,3 +1,8 @@
 import requests
-r = requests.get('https://api.macaddress.io/v1?apiKey=at_8CnUDq5XAbNywiGrzj77SJEBkoQy2&output=json&search=44:38:39:ff:ef:57').json()
-print(r)
+
+mac_address=input('Please Enter MAC:')
+if len(mac_address)>0:
+    r = requests.get('https://api.macaddress.io/v1?apiKey=at_8CnUDq5XAbNywiGrzj77SJEBkoQy2&output=json&search='+mac_address).json()
+    print("%s MAC Address is Associated with  Company %s ." %(mac_address, r['vendorDetails']['companyName']))
+else :
+    print('Please Enter Proper MAC Address.')
